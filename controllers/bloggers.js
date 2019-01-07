a4c511229d97c3cd545334f19924881152df0591
Amanda <aeverett1317@gmail.com>
2019-01-07 16:28:56 -0700

    fixed server file to include session

--- controllers/bloggers.js
+++ controllers/bloggers.js
@@ -5,8 +5,28 @@ module.exports = {
   index: function(req, res) {
     res.send("Hello");
   },
-  //this renders the adminstrator login page
+  //THIS RENDERS THE ADMIN LOGIN PAGE
   adminLoginPage: (req, res) => {
     res.render("admin-login");
-  }
+  },
+  //THIS LOGS THE ADMINISTRATOR IN
+  adminLogin: (req, res) => {
+    knex("bloggers")
+        .where("email", req.body.email)
+        .then(results => {
+            let patient = results[0];
+            if (!patient) {
+                req.flash('info', 'Could not locate a user with that email')
+                res.redirect("patients/login");
+            } else if (req.body.password && patient.password === req.body.password) {
+                req.session.user = null;
+                req.session.patient = patient;
+                req.session.role = 'patient';
+                res.redirect("/patients/appointment-page/confirmed");
+            } else {
+                req.flash('info', 'Invalid password')
+                res.redirect("patients/login");
+            }
+        })
+        .catch(err => console.log(err));
 };
