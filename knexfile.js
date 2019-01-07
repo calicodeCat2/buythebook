6f03f9a64894eb796f62c4dad16711a0816f5b28
David Murcek <dfmurcek@gmail.com>
2019-01-07 14:35:31 -0700

    Nice try.

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,7 @@ module.exports = {
     client: "pg",
     connection: {
       database: "buybookdata",
-      host: "127.0.0.1",
-      user: "postgres",
-      password: "12345"
+      host: "127.0.0.1"
     },
     migrations: {
       directory: __dirname + "/db/migrations"
